cf08de2cf49b4088d4caa956bf4b911f4abf4670
Mehmood <m.hassan.rahi@gmail.com>
2021-04-03 17:34:17 +0500

    * Lint the code

--- App.js
+++ App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  StatusBar,
-  Platform,
+    StyleSheet,
+    Text,
+    SafeAreaView,
+    StatusBar,
+    Platform,
 } from 'react-native';
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Hello World</Text>
+        </SafeAreaView>
+    );
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : null,
-  },
+    container: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : null,
+    },
 });
